d6a7e8a6e0849711e4c0739f52bfc7959d87bd08
RTiwari06 <darksoft52@gmail.com>
2026-06-10 17:46:41 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import App from '@/App.jsx';
 import './index.css';
 
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </React.StrictMode>,
 );
